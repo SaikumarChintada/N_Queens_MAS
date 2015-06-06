1f85d7e2857bbe62d35d72f0b8e71cef940bf42a
Saikumar <chintadasaikumar@gmail.com>
2015-06-06 15:24:05 +0530

    Partially Working Code Added

--- nqueensWorkingPartial.py
+++ nqueensWorkingPartial.py
@@ -23,6 +23,8 @@ class Agent(threading.Thread):
     KILL = False
 
     def __init__(self, network, board_size):
+        self.counter = 1
+        #create agent's context
         self.id = board_size - Agent.next_id  
         print "initiation of agent",self.id
         super(Agent, self).__init__(name=self.id)
@@ -30,93 +32,110 @@ class Agent(threading.Thread):
         
         self.fname = 'agent' + str(self.id)
         self.outfile = open(self.fname , 'w')
-         
+
+
         self.successors = []
         self.parents = {}
-        self.current_message = Agent.WAIT_MESSAGE # need to use this in code
-        self.messages = []
-        self.agent_view = {}    # for fast look up
-        self.no_goods = {}    
 
-        self.domain = [[self.id-1, i] for i in range(board_size)]
-        self.current_pos = 0
-        self.curr_pointer = 0
+        self.messages = []
+        self.agent_view = {}
+        self.no_goods = {-1:{0:[-1,-1]}}
+        self.recent_no_good = []
+
+        self.domain = [[self.id-1,i] for i in range(board_size)]
+        self.board_size = board_size
         self.assignment = self.domain[0]
         self.active = True
-        self.settled = False
-        self.board_s = board_size
 
         self.network = network
         self.network.add_agent(self)
-         
     
+    def __str__(self):
+        print 'Agent '+str(self.id)+'-----------------'
+        print 'pos',self.assignment
+        print 'msg',self.messages
+        print 'view',self.agent_view
+        print 'no_goods',self.no_goods
+        ls = '---------------------------------------'    
+        return ls
     #=============================================================
     def process_messages(self):
         m = self.messages.pop(0)
         if m[1] == Agent.OK_MESSAGE:
-            self.outfile.write('processing ok? msg\n')
+            self.outfile.write('processing ok? msg from agent'+str(m[0])+'\n')
             self.process_ok_message(m)
         elif m[1] == Agent.NO_GOOD:
-            self.outfile.write('processing no_good msg\n')
+            self.outfile.write('processing no_good msg recieved from agent'+str(m[0])+'\t'+str(m)+'\n')
             self.process_no_good_message(m)
 
     def process_ok_message(self, m):
+        #self.counter = 0
         self.agent_view[m[0]] = m[2]  #(agent : pos)
         self.check_agent_view()
 
     def process_no_good_message(self, m):
-        self.no_goods[len(self.no_goods)] = m[2]
-        #dynamic linking
-        print m[2]
-        for _id in m[2].keys():
-            if _id not in self.agent_view.keys() and _id != self.id:
-                #add link to it
-                a = self.network.get_agent(_id)
-                print 'saikumar',a
-                self.parents[_id] = a 
-                a.successors.append(self)
-                if _id != self.id:
-                    self.agent_view[_id] = a.assignment
-        self.check_agent_view()
+        if not m[2] in self.no_goods.values():
+            self.no_goods[len(self.no_goods)] = m[2]
+        self.recent_no_good = m[2]
+        present = True
+        if self.considered():
+            present = self.set_assignment()
+
+        if not present:
+            self.outfile.write('2domain exhausted --> backtrack\n')
+            self.backtrack()
     #==================================================================================
     def backtrack(self):
-        print 'backtracking'
-        self.outfile.write('backtracking\n')
         no_good = copy.copy(self.agent_view)
         if no_good:
-            t =  max(self.agent_view.keys())
-            self.outfile.write('sending no_goods to agent '+str(t)+'\n')
+            t = max(self.agent_view.keys())
+            self.outfile.write('sending no_goods to agent '+str(t)+'\t'+str(self.agent_view)+'\n')
             self.send_message(Agent.NO_GOOD,t,no_good)
             self.agent_view.pop(t)
             self.check_agent_view()
         else:
             self.outfile.write('Ended here\n')
+            print "end here"
+
+                
     #==================================================================================
     # else part need to be chanegd : if consistent do nothing
     def check_agent_view(self):
         self.outfile.write('check_agent_view\t: '+str(self.agent_view)+'\n')
-        if not self.consistent_check(self.assignment) or not self.check_no_goods(self.assignment):
+        if not self.consistent_check(self.assignment):
             self.outfile.write('not consistent\t: '+str(self.agent_view)+'\t'+str(self.assignment)+'\n')
+            present = True
             present = self.set_assignment()
             if not present:
-                self.outfile.write('domain exhausted --> backtrack\n')                     #checking the domain
-                self.backtrack()
-            else:
-                self.outfile.write('after chech_agent_view\t: '+str(self.assignment)+'\n')
+                print '-------domain exhausted for agent'+str(self.id)+' '+str(self.assignment)
+                self.outfile.write('domain exhausted --> backtrack\n')
+                if self.counter <=5:
+                    self.outfile.write('therefore backtracking\n')
+                    self.counter +=1
+                    self.backtrack()
+                else:
+                    self.outfile.write('bactracking limit exceeded\n')
+        self.outfile.write('after chech_agent_view\t: '+str(self.assignment)+'\n')
 
-    def set_assignment(self):
+    def set_assignment(self,flag = 0):
         d = False
-        #all constraints --> saisfied and agent_view --> inconsistent with no_goods
         for i in self.domain:
-            
-            if self.consistent_check(i) and self.check_no_goods(i):
+            if self.consistent_check(i) and i[1]!= self.assignment[1] and self.check_no_goods(i):
                 self.assignment = i
                 d = True
                 break
         if d:
+            self.outfile.write('sending ok? msg\n')
             self.send_message(Agent.OK_MESSAGE)
         return d
-    
+    def considered(self):
+        d = True
+        ls = [i for i in range(1,self.id)]
+        for i in ls:
+            if i not in self.agent_view.keys():
+                d = False
+                break
+        return d
     def consistent_check(self, val):
         #print 'consistency checking : agent '+str(self.id)+'\n'
         d = True
@@ -126,21 +145,30 @@ class Agent(threading.Thread):
                 break
         return d
 
-    def partof(self,d,D):
-        print 'here is the problem',d,D
-        flag = True
-        for i in d.keys():
-            if D[i][1] != d[i][1] and D[i][0]!= d[i][0]:
-                flag = False
-        return flag
-
     # updates the position (val) and compares the agent_view to the no_goods
     def check_no_goods(self, val):
+        mview = copy.copy(self.agent_view)
+        mview[self.id] = val
+        if mview in self.no_goods.values():
+            return False
+        return True
+        
+    def check(self,aview):
         d = True
-        for aview in self.no_goods:
-            for i in aview.keys():
-                if i == self.id and aview[i] == val:
-                    d = False 
+        for i in self.agent_view.keys():
+            if i in aview.keys() and self.agent_view[i][1] != aview[i][0]:
+                d = True
+            else:
+                d = False
+                break
+        return d
+    def check_no_goods2(self, val):
+        d = True
+        for i in self.agent_view.values():
+            for aview in self.no_goods.values():
+                if val in aview.values() and aview[self.id] == val and i in aview.values():
+                    d  = False
+                    self.outfile.write('check_no_goods\t: Failed -> change the assignment\n')
                     break
         return d
  
@@ -169,38 +197,30 @@ class Agent(threading.Thread):
     def init(self):
         self.outfile.write('sending ok? msg\n')
         self.send_message(Agent.OK_MESSAGE)
+        #print str(self)
 
     def run(self):
-        
+
         self.outfile.write(self.fname +' is running\n')
-        #if self.id >0:
-        #    self.outfile.write('haha '+str(self.parents[Thread-1])+'\n')
-        #'''
         int = 1
         while  int < 1000000:
             int += 1
+            #self.outfile.write('state\t: '+str(self.active)+'\n')
             if self.active:
-                #if it has some messages -> run 
-                while len(self.messages) != 0:
+                while len(self.messages) > 0:
                     self.process_messages()
+                    self.outfile.write('-----------------------------------\n')
                     self.outfile.write('pos\t\t: '+str(self.assignment)+'\n')
                     self.outfile.write('message\t: '+str(self.messages)+'\n')
                     self.outfile.write('agent_view\t: '+str(self.agent_view)+'\n')
                     self.outfile.write('no_goods\t: '+str(self.no_goods)+'\n')
-                #if no constraint : send ok
-                '''
-                if len(self.no_goods) == 0:
-                    self.outfile.write('sending ok? msg\n')
-                    self.send_message(Agent.OK_MESSAGE)
-                '''
-                #self.check_agent_view()
+                    self.outfile.write('-----------------------------------\n')
                 if len(self.messages) == 0:
                     self.active = False
+                    #self.send_message(Agent.OK_MESSAGE)
             else:
-                #self.check_agent_view()
                 if len(self.messages) > 0:
                     self.active = True
-        #'''
 
 #================================================================================
 class Network:
@@ -224,64 +244,31 @@ class Network:
 
 
 #================================================================================
-def Solve_n_queens(b): # b is board size
-    
-    n = Network()
 
-    agents = [Agent(n, b) for _ in range(b)]
-
-
-
-    [i.init() for i in agents]
-
-    [i.start() for i in agents]
-
-    agents = agents[::-1]
-
-    #four = [1,3,0,2]
-    #five = [0,2,4,1,3]
-
-    s = True
-    while s:
-        t = [i.assignment for i in agents]
-        time.sleep(2)
-        print t
-        c = True
-        #if all the agents are setteled then exit
-        for i in agents:
-            c = c and i.settled
-            if c:
-                break
-        if c:
-            break
-    #[i.join() for i in agents]
-    #program execution completed 
-    print ("==================================================================")
-
-def Solve_n_queens2(b):
+def Solve_n_queens(b):
 
     n = Network()
 
     agents = [Agent(n, b) for _ in range(b)]
 
     [i.outfile.write('parents \t: '+str(i.parents)+'\n') for i in agents]
-
     [i.outfile.write('successors\t: '+str(i.successors)+'\n') for i in agents]
+    [i.outfile.write('-------------------------------------------\n') for i in agents]
 
     [i.init() for i in agents]
 
     print agents
 
+    for i in agents:
+        print str(i)
+
     [i.outfile.write('messages\t: '+str(i.messages)+'\n') for i in agents]
 
     [i.start() for i in agents]
 
+
+
 if __name__ == '__main__':
     n = input('Enter a number : ')
-    Solve_n_queens2(n)
+    Solve_n_queens(n)
 
-'''
-    idea :  init lower priority agents first . put them in network.
-            make links to all the lower agents and the current agents.
-
-'''
